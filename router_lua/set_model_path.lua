0e9b8fce1b6994727c039a1d1a72902492ac7df7
Yue Di <dy1901@gmail.com>
2015-04-16 21:22:26 +0800

    refine

--- router_lua/set_model_path.lua
+++ router_lua/set_model_path.lua
@@ -3,52 +3,61 @@ local args = ngx.req.get_uri_args()
 local const = ngx.shared.const
 local model_path = ngx.shared.model_path
 
+if args.path == true then
+    args.path = ''
+end
+
 if args.model_name and args.server_name and args.host and args.path then
 
-    --if model_path:get(arg.modle_name) ==
+    if model_path:get(args.model_name) ~= args.host..args.path then
 
-    local mysql = require "resty.mysql"
-    local db, err = mysql:new()
-    if not db then
-        ngx.say("failed to instantiate mysql: ", err)
-        return
-    end
+        --if model_path:get(arg.modle_name) ==
 
-    db:set_timeout(1000) -- 1 sec
+        local mysql = require "resty.mysql"
+        local db, err = mysql:new()
+        if not db then
+            ngx.say("failed to instantiate mysql: ", err)
+            return
+        end
 
-    -- or connect to a unix domain socket file listened
-    -- by a mysql server:
-    --     local ok, err, errno, sqlstate =
-    --           db:connect{
-    --              path = "/path/to/mysql.sock",
-    --              database = "ngx_test",
-    --              user = "ngx_test",
-    --              password = "ngx_test" }
+        db:set_timeout(1000) -- 1 sec
 
-    local ok, err, errno, sqlstate = db:connect{
+        -- or connect to a unix domain socket file listened
+        -- by a mysql server:
+        --     local ok, err, errno, sqlstate =
+        --           db:connect{
+        --              path = "/path/to/mysql.sock",
+        --              database = "ngx_test",
+        --              user = "ngx_test",
+        --              password = "ngx_test" }
 
-        host = const:get("mysql_host"),
-        port = const:get("mysql_port"),
-        database = const:get("mysql_database"),
-        user = const:get("mysql_user"),
-        password = const:get("mysql_password"),
-        max_packet_size = const:get("mysql_max_packet_size") }
+        local ok, err, errno, sqlstate = db:connect{
 
-    if not ok then
-        ngx.say("failed to connect: ", err, ": ", errno, " ", sqlstate)
-        return
-    end
+            host = const:get("mysql_host"),
+            port = const:get("mysql_port"),
+            database = const:get("mysql_database"),
+            user = const:get("mysql_user"),
+            password = const:get("mysql_password"),
+            max_packet_size = const:get("mysql_max_packet_size") }
 
-    local res, err, errno, sqlstate =
+        if not ok then
+            ngx.say("failed to connect: ", err, ": ", errno, " ", sqlstate)
+            return
+        end
+
+        local res, err, errno, sqlstate =
         db:query("INSERT INTO model_paths (model_name, server_name, host, path, created_at, updated_at) VALUES (".."'"..args.model_name.."','"..args.server_name.."','"..args.host.."','"..args.path.."','"..ngx.localtime().."','"..ngx.localtime().."') ON DUPLICATE KEY UPDATE server_name='"..args.server_name.."', host='"..args.host.."', path='"..args.path.."', updated_at='"..ngx.localtime().."'")
-    if not res then
-        ngx.say("bad result: ", err, ": ", errno, ": ", sqlstate, ".")
-        return
-    else
-        model_path:set(args.model_name, args.host..args.path)
-    end
+        if not res then
+            ngx.say("bad result: ", err, ": ", errno, ": ", sqlstate, ".")
+            return
+        else
+            model_path:set(args.model_name, args.host..args.path)
+        end
 
-    ngx.say("success")
+        ngx.say("success")
+    else
+        ngx.say("path exists, not modified")
+    end
 
 else
     ngx.say("model_name, server_name, host and path must be present all together")
